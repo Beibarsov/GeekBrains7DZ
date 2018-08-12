a9246fb9f45d2b4c570c2a605ed4c7c9069cc7ca
Виталий Никонов <BarsStudio@gmail.com>
2018-08-12 15:20:53 +0800

    1

--- GeekBrains7DZ/MainWindow.xaml.cs
+++ GeekBrains7DZ/MainWindow.xaml.cs
@@ -20,9 +20,60 @@ namespace GeekBrains7DZ
     /// </summary>
     public partial class MainWindow : Window
     {
+        int ticket = 0;
+        Random random = new Random();
+        int winResult = 0;
+        int prevresult = 0;
+
         public MainWindow()
         {
+           
             InitializeComponent();
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            prevresult = int.Parse(LabelResult.Content.ToString());
+            LabelResult.Content = (int.Parse(LabelResult.Content.ToString()) + 1).ToString();
+            ticket++;
+
+        }
+        private void Button_Clickx2(object sender, RoutedEventArgs e)
+        {
+            prevresult = int.Parse(LabelResult.Content.ToString());
+            LabelResult.Content = (int.Parse(LabelResult.Content.ToString()) * 2).ToString();
+            ticket++;
+        }
+        private void Button_Clickreset(object sender, RoutedEventArgs e)
+        {
+            prevresult = int.Parse(LabelResult.Content.ToString());
+            LabelResult.Content = 0;
+            ticket++;
+        }
+
+        private void Write_Ticket(object sender, RoutedEventArgs e)
+        {
+            ticketCounter.Content = ticket;
+            if ((LabelWinResult.Content.ToString() == LabelResult.Content.ToString()))
+                MessageBox.Show($"Победа за {ticket.ToString()} ходов");
+        }
+
+        private void NewGame(object sender, RoutedEventArgs e)
+        {
+            winResult = random.Next(2,10);
+            LabelWinResult.Content = winResult;
+            ticket = 0;
+            LabelResult.Content = 0;
+            ticketCounter.Content = ticket;
+
+        }
+
+        private void Button_reset(object sender, RoutedEventArgs e)
+        {
+            LabelResult.Content = prevresult;
+            ticket++;
         }
     }
 }
